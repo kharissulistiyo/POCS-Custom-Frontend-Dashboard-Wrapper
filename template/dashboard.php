780a924e8110e385e56b310418d43480b74c8d5d
Kharis Sulistiyono <kharisblank@gmail.com>
2024-01-29 15:48:48 +0700

    Update dashboard main template

--- template/dashboard.php
+++ template/dashboard.php
@@ -1,3 +1,40 @@
+<?php
+
+if( pocscd_lock_page() ) {
+	do_action('pocscd_display_lock_page');
+	return;
+} 
+
+$query_vars = pocscd_app_query_vars();
+
+$page_title = !empty($query_vars['app_page_title']) ? $query_vars['app_page_title'] : '';
+
+global $wp_query;
+
+$wp_query->set('post_type', 'page');
+$wp_query->set('page_id', poscscd_dashboard_page_id());
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+	
+</head>
+
+<body <?php body_class(); ?>>
+
 <?php 
 
-echo __('Your frontend dashboard template goes here. Start editing this file.', 'pocscd');
+do_action('pocscd'); 
+
+?>>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
